5250b6f2182321ac67584ef32b7b4ac054eab266
shinhagunn <shinhagunn@gmail.com>
2021-11-07 00:41:26 +0700

    first upload

--- components/Login.vue
+++ components/Login.vue
@@ -1,37 +1,47 @@
 <template>
-  <div class="login">
-    <div class="logo">
-      <h1 class="logo-name">My music</h1>
+  <div class="login-block">
+    <div class="login-main">
+      <div class="login-another">
+        <p class="register">
+          Don't have an account yet? <a href="">Create one</a>
+        </p>
+      </div>
+
+      <div class="login-form">
+        <h2 class="title">Login to MyMusic</h2>
+        <form action="">
+          <div class="form-block">
+            <label for="iemail">Email</label>
+            <input
+              class="form-control"
+              id="iemail"
+              type="email"
+              name="email"
+              placeholder="Email"
+            />
+          </div>
+          <div class="form-block">
+            <label for="ipassword">Password</label>
+            <input
+              class="form-control"
+              id="ipassword"
+              type="password"
+              name="password"
+              placeholder="Password"
+            />
+          </div>
+          <div class="form-block">
+            <input
+              class="form-control btn"
+              type="submit"
+              name="submit"
+              value="Sign In"
+            />
+          </div>
+        </form>
+        <a href="" class="forgot">Forgot Password?</a>
+      </div>
     </div>
-
-    <form action="" class="form">
-      <div class="form-block">
-        <input
-          class="form-control"
-          type="email"
-          name="email"
-          placeholder="Email"
-        />
-      </div>
-
-      <div class="form-block">
-        <input
-          class="form-control"
-          type="password"
-          name="password"
-          placeholder="Password"
-        />
-      </div>
-
-      <div class="form-block">
-        <input
-          class="form-control bton"
-          type="submit"
-          name="sub"
-          value="Login"
-        />
-      </div>
-    </form>
   </div>
 </template>
 
@@ -39,61 +49,98 @@
 import { Component, Vue } from 'nuxt-property-decorator'
 
 @Component({})
-export default class Login extends Vue {
-  ten = 'hoang'
-}
+export default class Login extends Vue {}
 </script>
 
 <style lang="less">
-* {
-  padding: 0;
-  margin: 0;
-  box-sizing: border-box;
-  font-family: Arial, Helvetica, sans-serif;
-}
+.login-block {
+  position: absolute;
+  right: 0;
+  padding: 60px;
+  width: 50%;
+  height: 100%;
+  background-color: #fff;
 
-.login{
-    margin: 50px auto;
-    width: 350px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-}
+  a {
+    text-decoration: none;
+    color: rgb(119, 85, 229);
+  }
 
-.logo{
-    .logo-name{
-        padding: 20px;
-        font-size: 40px;
-    }
-}
+  a:hover {
+    text-decoration: underline;
+  }
 
-.form{
-    .form-control{
-        margin-bottom: 20px;
-        padding: 0 20px;
-        width: 350px;
-        height: 50px;
-        border: 1px solid #000;
+  .login-main {
+    .login-another {
+      .register {
+        margin-bottom: 80px;
+        text-align: right;
+        font-size: 14px;
+      }
     }
 
-    .form-control:hover{
-        border-color: #29adff;
-    }
+    .login-form {
+      margin: 0 auto;
+      width: 450px;
 
-    .form-control:focus{
-        outline: none;
-        border-color: #29adff;
-    }
+      .title {
+        margin-bottom: 80px;
+        display: inline-block;
+        line-height: 36px;
+        font-size: 24px;
+        font-weight: 500;
+        color: #6039de;
+        border-bottom: solid 2px #6039de;
+      }
 
-    .bton{
-        background-color: #29adff;
-        color: #fff;
-        font-size: 18px;
-        border-color: #29adff;
-    }
+      .form-block {
+        margin: 18px 0;
 
-    .bton:hover{
-        cursor: pointer;
+        label {
+          margin-bottom: 8px;
+          display: inline-block;
+          font-size: 16px;
+        }
+
+        .form-control {
+          padding: 0 12px;
+          width: 100%;
+          height: 46px;
+          display: block;
+          background-color: #f5f7f9;
+          border: 1px solid #fff;
+          border-radius: 5px;
+          transition: all 0.3s;
+        }
+
+        .form-control:hover {
+          border: 1px solid #6039de;
+        }
+
+        .form-control:focus {
+          border: 1px solid #6039de;
+          outline: none;
+        }
+
+        .btn {
+          margin-top: 120px;
+          background-color: #7755e5;
+          border: none;
+          color: #fff;
+          transition: all 0.3s;
+        }
+
+        .btn:hover {
+          border: none;
+          cursor: pointer;
+          background-color: rgba(119, 85, 229, 0.85);
+        }
+      }
+
+      .forgot {
+        font-size: 14px;
+      }
     }
+  }
 }
 </style>
---
+++ pages/loginPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="loginPage">
+    <Login/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component({})
+export default class loginPage extends Vue {
+  
+}
+</script>
+
+<style lang="less">
+*{
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+}
+html{
+  background: url('../content/img/background_login.jpg') no-repeat;
+  background-size: cover;
+  font-family: Arial, Helvetica, sans-serif;
+}
+</style>
